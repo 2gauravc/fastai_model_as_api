df9858dbfeb0d065dbf3c4a8256a2017b1257868
Ubuntu <ubuntu@ip-172-31-19-236.ec2.internal>
2020-12-05 02:15:59 +0000

    create model dir

--- model_load.py
+++ model_load.py
@@ -4,7 +4,7 @@ import pathlib
 from pathlib import Path
 pathlib.WindowsPath = pathlib.PosixPath
 print ("fastai: version {}".format(fastai.__version__))
-model_path = Path('./efficientnet_lite0__v4.2.pkl')
+model_path = Path('model/efficientnet_lite0__v4.2.pkl')
 #model_path = 'efficientnet_lite0__v4.2.pkl'
 learn_inf = load_learner(model_path)
 
